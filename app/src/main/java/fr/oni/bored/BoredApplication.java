dba783d7b67f8638724e3f11a20e20fe7ec6ea32
Daniel Chesters <daniel.chesters@gmail.com>
2015-05-09 18:24:49 +0200

    Add leakCanary (and update some libraries)

---
+++ app/src/main/java/fr/oni/bored/BoredApplication.java
@@ -0,0 +1,13 @@
+package fr.oni.bored;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class BoredApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
